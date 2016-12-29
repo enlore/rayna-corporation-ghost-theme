022dba31a7ffabb767d21252e96328b52dec0b84
enlore <n.e.lorenson@gmail.com>
2016-12-29 11:53:37 -0600

    Updates to theme pages

--- content/themes/rayna-corp-theme/assets/js/index.js
+++ content/themes/rayna-corp-theme/assets/js/index.js
@@ -1,2 +1,18 @@
 //var $postContent = $(".post-content");
 //$postContent.fitVids();
+//
+;(function (ctx) {
+  ctx.rayna = ctx.rayna || {};
+
+  ctx.rayna.parseJSON = function parseJSON(response) { return response.json(); };
+  ctx.rayna.checkStatus = function checkStatus(response) {
+      if (response.status >= 200 && response.status < 300) {
+        return response;
+      } else {
+        var error = new Error(response.statusText);
+        error.response = response;
+        throw error;
+      }
+    };
+
+})(this);
